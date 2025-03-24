4a1d34752c929095bba5cbc9ab7c7a5fb1472a50
M2 <sahil.kumar@geeky.dev>
2025-03-24 08:54:58 +0530

    removed unused code

--- apps/http-backened/src/routes/room-route.ts
+++ apps/http-backened/src/routes/room-route.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { createRoom, getChat,getSlug } from "../controller/room-controller";
 import { authMiddleware } from '../middleware/auth';
-import { get } from 'mongoose';
 const RoomRouter: express.Router = express.Router();
 
 // Direct middleware approach
